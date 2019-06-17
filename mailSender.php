467627cb8abc2c0674a0b3c03ff1934c3f760a77
AlexanderAbdulla <alexander.amadeus.abdulla@gmail.com>
2019-06-16 21:31:18 -0700

    Added Bootstrap and Support for GMAIL and HOTMAIL

--- mailSender.php
+++ mailSender.php
@@ -1,12 +1,5 @@
 <?php
 session_start();
-//echo 'start of the script';
-$username = $_GET['username'];
-$password = $_GET['password'];
-$body = $_GET['body'];
-$subject = $_GET['subject'];
-$to_address = $_GET['to_address'];
-
 
 use PHPMailer\PHPmailer\PHPMailer;
 use PHPMailer\PHPmailer\Exception;
@@ -14,12 +7,17 @@ require 'PHPMailer\PHPMailer\src\Exception.php';
 require 'PHPMailer\PHPMailer\src\PHPMailer.php';
 require 'PHPMailer\PHPMailer\src\SMTP.php';
 
+$username = $_GET['username'];
+$password = $_GET['password'];
+$body = $_GET['body'];
+$subject = $_GET['subject'];
+$to_address = $_GET['to_address'];
+$host = $_GET['host'];
 
-//require 'PHPMailer/vendor/autoload.php';
 $mail = new PHPMailer;
 $mail->IsSMTP();
 $mail->SMTPDebug = 1;
-$mail->Host = "smtp.live.com";
+$mail->Host = $host;
 
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587; // or 587
@@ -28,7 +26,7 @@ $mail->Username = $username;
 $mail->Password = $password;
 
 $mail->isHTML(false);
-$mail->setFrom('cragzeek@hotmail.com');
+$mail->setFrom($username);
 $mail->addAddress($to_address);
 $mail->addReplyTo($to_address);
 $mail->Subject = $subject;
@@ -38,15 +36,13 @@ if(!$mail->send()) {
     //echo "Mailer Error: " . $mail->ErrorInfo;
     $_SESSION['success'] = false;
 
-//    echo 'did we even make it here?';
+//echo 'did we even make it here?';
 } else {
      $_SESSION['success'] = true;
     //echo "Message has been sent!";
 }
 
-
 header('Location: index.php');
-//echo 'nothing is broken yet';
 
 ?>
 
